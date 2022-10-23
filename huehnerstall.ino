24d18ce3a29ba45b4734e0e4576636c08e9f0177
Mekhail Doumet <michaledommet@hotmail.com>
2022-10-23 22:25:27 +0200

    add circuit diagram and cleanup

--- huehnerstall.ino
+++ huehnerstall.ino
@@ -2,7 +2,6 @@
 #include <Wire.h>
 
 // Define what pins are used
-const int DoorUpSensorPin = 4;   // reedswitch Door open
 const int DoorDownMotorPin = 2; // wind motor down
 const int DoorUpMotorPin = 3;    // wind motor up
 bool DoorOpen = false;
@@ -14,15 +13,7 @@ void setup()
     Serial.begin(9600);
     pinMode(DoorUpMotorPin, OUTPUT);
     pinMode(DoorDownMotorPin, OUTPUT);
-
-    // switches setup
-    pinMode(DoorUpSensorPin, INPUT_PULLUP);
-    if (digitalRead(DoorUpSensorPin) == HIGH)
-    {
-        Serial.println("Door is open");
-    }
     delay(3000);
-    // TO DO , if the Door is hanged some where , we need to open it and then close it .
 }
 
 // the loop function runs over and over again until power down or reset
@@ -31,8 +22,6 @@ void loop()
     int brightens = analogRead(A1);
     delay(2000);
     Serial.println(brightens);
-    // lets start comparisons, if the Door should be up....
-    // delay of 30 minutes after sunset time to make sure all chickens are inside before closing the Door.
     if (brightens > 400 && DoorOpen == false)
     {
         Serial.println("Door should be up");

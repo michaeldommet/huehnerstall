e183e0dfc277f284314ee1e142b1eb4782ecba5c
Mekhail Doumet <10365842+michaeldommet@users.noreply.github.com>
2023-12-21 21:45:48 +0100

    adding rtc module

--- huehnerstall.ino
+++ huehnerstall.ino
@@ -1,47 +1,79 @@
-
 #include <Wire.h>
+#include <RTClib.h>
 
-// Define what pins are used
+RTC_DS3231 rtc;
+
+const int relayPin = 4; // Connect the relay module signal pin to digital pin 4
+
 const int DoorDownMotorPin = 2; // wind motor down
-const int DoorUpMotorPin = 3;    // wind motor up
+const int DoorUpMotorPin = 3;   // wind motor up
 bool DoorOpen = false;
 
-// the setup function runs once when you press reset or power the board
 void setup()
 {
-    // Set the mode the pins will operate in.
     Serial.begin(9600);
+
+    if (!rtc.begin())
+    {
+        Serial.println("Couldn't find RTC");
+        while (1)
+            ;
+    }
+
+    if (rtc.lostPower())
+    {
+        Serial.println("RTC lost power, let's set the time!");
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+
+    pinMode(relayPin, OUTPUT);
     pinMode(DoorUpMotorPin, OUTPUT);
     pinMode(DoorDownMotorPin, OUTPUT);
     delay(3000);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop()
 {
+    DateTime now = rtc.now();
+
+    // Set the activation time every day between 13:00 and 19:00
+    DateTime startTime(now.year(), now.month(), now.day(), 13, 0, 0);
+    DateTime endTime(now.year(), now.month(), now.day(), 19, 0, 0);
+
+    // Check if the current time is within the specified range
+    if (now >= startTime && now <= endTime)
+    {
+        // Activate the relay for the entire duration
+        digitalWrite(relayPin, HIGH);
+    }
+    else
+    {
+        // Deactivate the relay outside the specified range
+        digitalWrite(relayPin, LOW);
+    }
+
     int brightens = analogRead(A1);
     delay(2000);
     Serial.println(brightens);
+
     if (brightens > 400 && DoorOpen == false)
     {
         Serial.println("Door should be up");
         raiseDoor();
         stopDoor();
         DoorOpen = true;
-     
     }
-    else if (brightens < 300  && DoorOpen == true)
+    else if (brightens < 200 && DoorOpen == true)
     {
         Serial.println("Door should be down");
         lowerDoor();
         stopDoor();
         DoorOpen = false;
     }
-    else {
+    else
+    {
         Serial.println("I am not doing anything");
     }
-
-    
 }
 
 // Wind the Door Up
@@ -50,7 +82,7 @@ void raiseDoor()
     digitalWrite(DoorUpMotorPin, HIGH);
     digitalWrite(DoorDownMotorPin, LOW);
     Serial.println("Door Raising");
-    delay(12000);
+    delay(11000);
 }
 
 // Wind The Door Down
@@ -59,7 +91,7 @@ void lowerDoor()
     digitalWrite(DoorDownMotorPin, HIGH);
     digitalWrite(DoorUpMotorPin, LOW);
     Serial.println("Door Lowering");
-    delay(12000);
+    delay(10900);
 }
 
 void stopDoor()
